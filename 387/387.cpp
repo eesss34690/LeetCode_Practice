6e7c7f768d8f1bf24060513fbad912be309d7d60
eesss34690 <eesss.ee07@nctu.edu.tw>
2021-01-24 12:04:48 +0800

    2021/jan./21, 387

---
+++ 387/387.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        int alpha[26]= {0};
+        for (int i= 0; i< s.length(); i++)
+        {
+            alpha[s[i]- 'a']++;
+        }
+        for (int i= 0; i< s.length(); i++)
+        {
+            if(alpha[s[i]- 'a']== 1)
+                return i;
+        }
+        return -1;
+    }
+};
