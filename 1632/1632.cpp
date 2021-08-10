52bb745c86dc7518cfff15efb47ee90bad85cd58
eli6 <eli6@logitech.com>
2021-08-10 09:48:34 +0800

    2021/aug./9,1632

---
+++ 1632/1632.cpp
@@ -0,0 +1,43 @@
+class UnionFind {
+public:
+    unordered_map<int, int> parent;
+    int Find(int u) {
+        if (u == parent[u]) return u;
+        return parent[u] = Find(parent[u]);
+    }
+    void Union(int u, int v) {
+        if (parent.count(u) == 0) parent[u] = u;
+        if (parent.count(v) == 0) parent[v] = v;
+        int pu = Find(u), pv = Find(v);
+        if (pu != pv) parent[pu] = pv;
+    }
+};
+class Solution {
+public:
+    vector<vector<int>> matrixRankTransform(vector<vector<int>>& matrix) {
+        int m = matrix.size(), n = matrix[0].size();
+        map<int, vector<pair<int, int>>> groupByValue;
+        for (int r = 0; r < m; ++r)
+            for (int c = 0; c < n; ++c)
+                groupByValue[matrix[r][c]].push_back({r, c});
+
+        vector<int> rank(m+n, 0);
+        for (auto const& [_, cells]  : groupByValue) {
+            UnionFind uf;
+            for (auto const& [r, c] : cells)
+                uf.Union(r, c + m);
+
+            unordered_map<int, vector<int>> groups;
+            for (auto const& [u, _] : uf.parent) 
+                groups[uf.Find(u)].push_back(u);
+
+            for (auto const& [_, group] : groups) {
+                int maxRank = 0;
+                for (int i : group) maxRank = max(maxRank, rank[i]);
+                for (int i : group) rank[i] = maxRank + 1;
+            }
+            for (auto const& [r, c] : cells) matrix[r][c] = rank[r];
+        }
+        return matrix;
+    }
+};
