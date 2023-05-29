702f954410fa189eab24ea728d4abeea5198447f
Esme <eesss.ee07@nctu.edu.tw>
2023-05-29 08:32:51 +0800

    2023/may/29,1603

---
+++ 1603/1603.cpp
@@ -0,0 +1,46 @@
+class ParkingSystem {
+private:
+    int b, m, s;
+public:
+    ParkingSystem(int big, int medium, int small) {
+        b = big;
+        m = medium;
+        s = small;
+    }
+
+    bool addCar(int carType) {
+        switch (carType)
+        {
+        case 1:
+            if (b > 0)
+            {
+                b--;
+                return true;
+            }
+            else return false;
+            break;
+        case 2:
+            if (m > 0)
+            {
+                m--;
+                return true;
+            }
+            else return false;
+            break;
+        case 3:
+            if (s > 0)
+            {
+                s--;
+                return true;
+            }
+            else return false;
+        }
+        return false;
+    }
+};
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * ParkingSystem* obj = new ParkingSystem(big, medium, small);
+ * bool param_1 = obj->addCar(carType);
+ */
