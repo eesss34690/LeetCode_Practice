cb039875ba3156e889ca75084420eb11430e555b
Esme <eesss.ee07@nctu.edu.tw>
2023-06-19 09:03:18 +0800

    2023/jun./19,1732

---
+++ 1732/1732.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int ans = max(gain[0], 0);
+        for (int i = 1; i < gain.size(); i++)
+        {
+            gain[i] += gain[i - 1];
+            ans = max(ans, gain[i]);
+        }
+        return ans;
+    }
+};
